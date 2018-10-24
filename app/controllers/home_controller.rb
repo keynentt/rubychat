e6d85700e757b05a50e2ffa2883b1c112948bed0
keynentt <keynentt@gmail.com>
2018-10-24 20:05:05 +0800

    Deploying to Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
       def index
-        @chats = Chat.all.order("created_at DESC")
+      	@limit = 10
+		offset = params[:offset].to_i * @limit 
+		
+        @chats = Chat.all.order("created_at DESC").limit(5)
       end
     end
 
